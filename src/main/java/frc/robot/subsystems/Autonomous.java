f830511b35ad426c9eb3d2501c7d6d5cd7331bb0
PCRobotics <pcrobotoics@apps.district279.org>
2023-02-18 15:16:35 -0600

    Uneeded Imports

--- src/main/java/frc/robot/subsystems/Autonomous.java
+++ src/main/java/frc/robot/subsystems/Autonomous.java
@@ -4,13 +4,10 @@
 
 package frc.robot.subsystems;
 
-import javax.swing.text.Position;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
