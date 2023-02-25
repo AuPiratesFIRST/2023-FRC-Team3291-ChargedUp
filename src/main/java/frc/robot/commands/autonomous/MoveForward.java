661d031ce25ef6bdad251bbd770d0950cf83d907
PCRobotics <pcrobotoics@apps.district279.org>
2023-02-25 16:47:56 -0600

    motor fix

--- src/main/java/frc/robot/commands/autonomous/MoveForward.java
+++ src/main/java/frc/robot/commands/autonomous/MoveForward.java
@@ -29,12 +29,13 @@ public class MoveForward extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-   driveTrainSubsystems.moveForwardOrBack(0, Constants.STOPPOWER);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+   driveTrainSubsystems.moveForwardOrBack(0, Constants.STOPPOWER);
+  }
 
   // Returns true when the command should end.
   @Override
